0fd95d02446e142c0365c1535bc62f73c5d8fc86
Milos Kostadinovski <milos.kostadinovski97@gmail.com>
2019-12-15 01:20:33 +0100

    Added solution for day 12

--- 5/5.php
+++ 5/5.php
@@ -1,11 +1,27 @@
 <?php
 
+function getParameter($program, $i, $parameterMode, $offset) {
+    $parameter = null;
+    if ($parameterMode == 0) {
+        if (isset($program[$program[$i + $offset]]))
+            $parameter = $program[$program[$i + $offset]]; 
+    } elseif ($parameterMode == 1) {
+        if (isset($program[$i + $offset]))
+            $parameter = $program[$i + $offset];
+    } elseif ($parameterMode == 2) {
+        if (isset($program[$i + $offset]))
+            $parameter = $program[$i + $offset];
+    } else {
+        $parameter = null;
+    }
+
+    return $parameter;
+}
+
 $file = fopen("5.txt","r");
 $line = fgets($file);
-//$line = "1002,4,3,4,33";
+
 $program = array_map('intval', explode(',', $line));
-//$program[1] = 12;
-//$program[2] = 2;
 
 for ($i = 0; $i < count($program); $i++) {
     $instruction      = strrev((string)$program[$i]);
@@ -14,52 +30,83 @@ for ($i = 0; $i < count($program); $i++) {
     $modeOfParameter2 = (int) substr($instruction, 3, 1) ?: 0;
     $modeOfParameter3 = (int) substr($instruction, 4, 1) ?: 0;
 
+    switch($modeOfParameter1) {
+        case 0:
+            if (isset($program[$program[$i + 1]]))
+                $parameter1 = $program[$program[$i + 1]]; 
+            break;
+        case 1:
+            if (isset($program[$i + 1]))
+                $parameter1 = $program[$i + 1];
+            break;
+        case 2:
+            if (isset($program[$program[$i + 1]]))
+                $parameter1 = $program[$program[$i + 1]];
+            break;
+    }
+
+    switch($modeOfParameter2) {
+        case 0:
+            if (isset($program[$program[$i + 2]]))
+                $parameter2 = $program[$program[$i + 2]]; 
+            break;
+        case 1:
+            if (isset($program[$i + 2]))
+                $parameter2 = $program[$i + 2];
+            break;
+        case 2:
+            if (isset($program[$program[$i + 2]]))
+                $parameter2 = $program[$program[$i + 2]];
+            break;
+    }
+
+    switch($modeOfParameter3) {
+        case 0:
+            if (isset($program[$i + 3]))
+                $parameter3 = $program[$i + 3]; 
+            break;
+        case 1:
+            if (isset($program[$program[$i + 3]]))
+                $parameter3 = $program[$program[$i + 3]];
+            break;
+        case 2:
+            if (isset($program[$program[$i + 3]]))
+                $parameter3 = $program[$program[$i + 3]];
+            break;
+    }
+
     if ($opcode === 1) {
-        $parameter1 = $modeOfParameter1 === 1 ? $program[$i + 1] : $program[$program[$i + 1]];
-        $parameter2 = $modeOfParameter2 === 1 ? $program[$i + 2] : $program[$program[$i + 2]];
-        $parameter3 = $modeOfParameter3 === 1 ? $program[$program[$i + 3]] : $program[$i + 3];
         $program[$parameter3] = $parameter1 + $parameter2;
         $i += 3;
 
     } elseif ($opcode === 2) {
-        $parameter1 = $modeOfParameter1 === 1 ? $program[$i + 1] : $program[$program[$i + 1]];
-        $parameter2 = $modeOfParameter2 === 1 ? $program[$i + 2] : $program[$program[$i + 2]];
-        $parameter3 = $modeOfParameter3 === 1 ? $program[$program[$i + 3]] : $program[$i + 3];
         $program[$parameter3] = $parameter1 * $parameter2;
         $i += 3;
 
     } elseif ($opcode === 3) {
         $handle = fopen ("php://stdin","r");
         echo 'Enter an integer: ';
-        $input = (int) fgets($handle);
+        $input = (int) 1;
         $program[$program[$i + 1]] = $input;
         $i += 1;
 
     } elseif ($opcode === 4) {
-        $parameter1 = $modeOfParameter1 === 1 ? $program[$i + 1] : $program[$program[$i + 1]] ;
         echo $parameter1 . PHP_EOL;
         $i += 1;
 
     } elseif ($opcode === 5) {
-        $parameter1 = $modeOfParameter1 === 1 ? $program[$i + 1] : $program[$program[$i + 1]];
-        $parameter2 = $modeOfParameter2 === 1 ? $program[$i + 2] : $program[$program[$i + 2]];
         if ($parameter1 !== 0)
             $i = $parameter2 - 1;
         else
             $i += 2;
 
     } elseif ($opcode === 6) {
-        $parameter1 = $modeOfParameter1 === 1 ? $program[$i + 1] : $program[$program[$i + 1]];
-        $parameter2 = $modeOfParameter2 === 1 ? $program[$i + 2] : $program[$program[$i + 2]];
         if ($parameter1 === 0)
             $i = $parameter2 - 1;
         else
             $i += 2;
 
     } elseif ($opcode === 7) {
-        $parameter1 = $modeOfParameter1 === 1 ? $program[$i + 1] : $program[$program[$i + 1]];
-        $parameter2 = $modeOfParameter2 === 1 ? $program[$i + 2] : $program[$program[$i + 2]];
-        $parameter3 = $modeOfParameter3 === 1 ? $program[$program[$i + 3]] : $program[$i + 3];
         if ($parameter1 < $parameter2)
             $program[$parameter3] = 1;
         else
@@ -67,9 +114,6 @@ for ($i = 0; $i < count($program); $i++) {
         $i += 3;
 
     } elseif ($opcode === 8) {
-        $parameter1 = $modeOfParameter1 === 1 ? $program[$i + 1] : $program[$program[$i + 1]];
-        $parameter2 = $modeOfParameter2 === 1 ? $program[$i + 2] : $program[$program[$i + 2]];
-        $parameter3 = $modeOfParameter3 === 1 ? $program[$program[$i + 3]] : $program[$i + 3];
         if ($parameter1 === $parameter2)
             $program[$parameter3] = 1;
         else
@@ -81,6 +125,4 @@ for ($i = 0; $i < count($program); $i++) {
     }
 }
 
-// var_dump($program);
-// echo $program[0] . PHP_EOL;
 fclose($file);
