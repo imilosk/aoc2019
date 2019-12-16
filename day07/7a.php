7e93185ae3a2bb99763af4825d09e72b0b6f7804
Milos Kostadinovski <milos.kostadinovski97@gmail.com>
2019-12-16 11:29:17 +0100

    Refactored solution for part one of day 7, removed test file

--- day07/7a.php
+++ day07/7a.php
@@ -1,83 +1,19 @@
 <?php
+    include '../Intcode/IntcodeComputer.php';
 
-function intcodeComputer($program, $inputSetting = null, $inputSignal) {
-    $firstArgument = true;
-    for ($i = 0; $i < count($program); $i++) {
-        $instruction      = strrev((string)$program[$i]);
-        $opcode           = (int) strrev( substr($instruction, 0, 2) );
-        $modeOfParameter1 = (int) substr($instruction, 2, 1) ?: 0;
-        $modeOfParameter2 = (int) substr($instruction, 3, 1) ?: 0;
-        $modeOfParameter3 = (int) substr($instruction, 4, 1) ?: 0;
+// } elseif ($opcode === 3) {
+//     if ($firstArgument && $inputSetting !== null) {
+//         $program[$program[$i + 1]] = $inputSetting;
+//         $firstArgument = false;
+//     }else {
+//         $program[$program[$i + 1]] = $inputSignal;
+//     }
+//     $i += 1;
     
-        if ($opcode === 1) {
-            $parameter1 = $modeOfParameter1 === 1 ? $program[$i + 1] : $program[$program[$i + 1]];
-            $parameter2 = $modeOfParameter2 === 1 ? $program[$i + 2] : $program[$program[$i + 2]];
-            $parameter3 = $modeOfParameter3 === 1 ? $program[$program[$i + 3]] : $program[$i + 3];
-            $program[$parameter3] = $parameter1 + $parameter2;
-            $i += 3;
-    
-        } elseif ($opcode === 2) {
-            $parameter1 = $modeOfParameter1 === 1 ? $program[$i + 1] : $program[$program[$i + 1]];
-            $parameter2 = $modeOfParameter2 === 1 ? $program[$i + 2] : $program[$program[$i + 2]];
-            $parameter3 = $modeOfParameter3 === 1 ? $program[$program[$i + 3]] : $program[$i + 3];
-            $program[$parameter3] = $parameter1 * $parameter2;
-            $i += 3;
-    
-        } elseif ($opcode === 3) {
-            if ($firstArgument && $inputSetting !== null) {
-                $program[$program[$i + 1]] = $inputSetting;
-                $firstArgument = false;
-            }else {
-                $program[$program[$i + 1]] = $inputSignal;
-            }
-            $i += 1;
-            
-        } elseif ($opcode === 4) {
-            $parameter1 = $modeOfParameter1 === 1 ? $program[$i + 1] : $program[$program[$i + 1]];
-            return $parameter1;
+// } elseif ($opcode === 4) {
+//     $parameter1 = $modeOfParameter1 === 1 ? $program[$i + 1] : $program[$program[$i + 1]];
+//     return $parameter1;
 
-        } elseif ($opcode === 5) {
-            $parameter1 = $modeOfParameter1 === 1 ? $program[$i + 1] : $program[$program[$i + 1]];
-            $parameter2 = $modeOfParameter2 === 1 ? $program[$i + 2] : $program[$program[$i + 2]];
-            if ($parameter1 !== 0)
-                $i = $parameter2 - 1;
-            else
-                $i += 2;
-    
-        } elseif ($opcode === 6) {
-            $parameter1 = $modeOfParameter1 === 1 ? $program[$i + 1] : $program[$program[$i + 1]];
-            $parameter2 = $modeOfParameter2 === 1 ? $program[$i + 2] : $program[$program[$i + 2]];
-            if ($parameter1 === 0)
-                $i = $parameter2 - 1;
-            else
-                $i += 2;
-    
-        } elseif ($opcode === 7) {
-            $parameter1 = $modeOfParameter1 === 1 ? $program[$i + 1] : $program[$program[$i + 1]];
-            $parameter2 = $modeOfParameter2 === 1 ? $program[$i + 2] : $program[$program[$i + 2]];
-            $parameter3 = $modeOfParameter3 === 1 ? $program[$program[$i + 3]] : $program[$i + 3];
-            if ($parameter1 < $parameter2)
-                $program[$parameter3] = 1;
-            else
-                $program[$parameter3] = 0;
-            $i += 3;
-    
-        } elseif ($opcode === 8) {
-            $parameter1 = $modeOfParameter1 === 1 ? $program[$i + 1] : $program[$program[$i + 1]];
-            $parameter2 = $modeOfParameter2 === 1 ? $program[$i + 2] : $program[$program[$i + 2]];
-            $parameter3 = $modeOfParameter3 === 1 ? $program[$program[$i + 3]] : $program[$i + 3];
-            if ($parameter1 === $parameter2)
-                $program[$parameter3] = 1;
-            else
-                $program[$parameter3] = 0;
-            $i += 3;
-            
-        } elseif ($opcode === 99) {
-            break;
-        }
-    }
-    return 'halt';
-}
 
 function getAllCombos($arr) {
     $combinations = array();
@@ -108,7 +44,22 @@ $settingCombinations = getAllCombos($possibleSettings);
 foreach ($settingCombinations as $settings) {
     $signal = 0;
     foreach ($settings as $setting) {
-        $signal = intcodeComputer($program, $setting, $signal);
+        $firstArgument = true;
+        $computer = new IntcodeComputer($program);
+        $computer->execute(
+            function () use (&$setting, &$signal, &$firstArgument) {
+                if ($firstArgument && $setting !== null) {
+                    $firstArgument = false;
+                    return $setting;
+                } else {
+                    return $signal;
+                }
+            },
+            function ($output) use (&$signal) {
+                $signal = $output;
+            }
+        );
+
     }
     $maxSignal = max($signal, $maxSignal);
 }
