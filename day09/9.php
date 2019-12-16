8f8712abc32822b3395789e2fbd5f1d0cde5c556
Milos Kostadinovski <milos.kostadinovski97@gmail.com>
2019-12-16 09:25:22 +0100

    Refactored solution for day 9 with Intcomputer

---
+++ day09/9.php
@@ -0,0 +1,33 @@
+<?php
+
+include '../Intcode/IntcodeComputer.php';
+
+$file = fopen("9.txt","r");
+$line = fgets($file);
+$program = array_map('intval', explode(',', $line));
+
+$computer = new IntcodeComputer($program);
+$generator = $computer->execute();
+
+while (!$computer->halt) {
+    $output = $generator->current();
+
+    // if - input, else - output
+    if ($output === 'Input') {
+        // part one
+        // $input = 1;
+        
+        // part two
+        $input = 2;
+
+        $generator->send($input);
+
+    } elseif ($output !== null) {
+        echo $output . PHP_EOL;
+
+        $generator->next();
+    }
+    
+}
+
+fclose($file);
