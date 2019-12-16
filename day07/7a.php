cf82961190f96a6d72b324b9dbe781d4cdfe3e95
Milos Kostadinovski <milos.kostadinovski97@gmail.com>
2019-12-16 11:42:01 +0100

    Removed commented code

--- day07/7a.php
+++ day07/7a.php
@@ -1,19 +1,5 @@
 <?php
-    include '../Intcode/IntcodeComputer.php';
-
-// } elseif ($opcode === 3) {
-//     if ($firstArgument && $inputSetting !== null) {
-//         $program[$program[$i + 1]] = $inputSetting;
-//         $firstArgument = false;
-//     }else {
-//         $program[$program[$i + 1]] = $inputSignal;
-//     }
-//     $i += 1;
-    
-// } elseif ($opcode === 4) {
-//     $parameter1 = $modeOfParameter1 === 1 ? $program[$i + 1] : $program[$program[$i + 1]];
-//     return $parameter1;
-
+include '../Intcode/IntcodeComputer.php';
 
 function getAllCombos($arr) {
     $combinations = array();
@@ -34,7 +20,6 @@ function getAllCombos($arr) {
 
 $file = fopen("7.txt", "r");
 $line = fgets($file);
-// $line = "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5";
 $program = array_map('intval', explode(',', $line));
 
 $possibleSettings = [0, 1, 2, 3, 4];
@@ -50,7 +35,7 @@ foreach ($settingCombinations as $settings) {
     for ($i = 0; $i < count($settings); $i++) {
         $setting = $settings[$i];
         $firstArgument = true;
-        
+
         $computers[$i]->execute(
             function () use (&$setting, &$signal, &$firstArgument) {
                 if ($firstArgument && $setting !== null) {
